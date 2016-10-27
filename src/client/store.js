40191bd148353dcc8cdb5e91800225fe900f7b33
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-10-27 21:17:26 +1000

    use the updated version of redux dev tools extension config

--- src/client/store.js
+++ src/client/store.js
@@ -5,10 +5,19 @@ import thunk from 'redux-thunk';
 import app from './reducers';
 
 const initialState = new Map();
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify here name, actionsBlacklist, actionsCreators and other options
+    }) : compose;
+/* eslint-enable */
 
 export default createStore(app,
   initialState,
-  compose(
+  composeEnhancers(
     applyMiddleware(thunk),
     window.devToolsExtension && process.env.NODE_ENV === 'development'
       ? window.devToolsExtension() : f => f
